cdb57db2b1145faddd9625a7d021ed5360e1c0a4
Jordan Sampson <jordansampson23@gmail.com>
2024-06-04 23:38:51 -0500

    -Center Logo - Left & right nav.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton, Tooltip } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, IconButton, Tooltip } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import { styled } from '@mui/system';
 
 const HeaderContainer = styled(AppBar)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
+  backgroundColor: theme.palette.secondary.main,
   marginBottom: theme.spacing(4),
+  padding: theme.spacing(2),
 }));
 
 const Header = ({ toggleTheme }) => {
   return (
     <HeaderContainer position="static">
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Wandering Wisdom
-        </Typography>
-        <Tooltip  title="Toggle Theme" >
-          <IconButton color="inherit" onClick={toggleTheme} >
-            <DarkModeOutlinedIcon />
-          </IconButton>
-        </Tooltip>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
+          <Button color="inherit">Link 1</Button>
+          <Button color="inherit">Link 2</Button>
+        </Box>
+
+        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+          <Typography variant="h6" sx={{ flexGrow: 1, textAlign: 'center' }}>
+            Wandering Wisdom
+          </Typography>
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, justifyContent: 'flex-end' }}>
+          <Button color="inherit">Link 3</Button>
+          <Button color="inherit">Link 4</Button>
+          <Tooltip title="Toggle Theme">
+            <IconButton color="inherit" onClick={toggleTheme}>
+              <DarkModeOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
       </Toolbar>
     </HeaderContainer>
   );
